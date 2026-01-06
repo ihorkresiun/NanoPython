7a201736fc34a0c7bb4f39fe659ac07abb1fc13c
Ihor Kresiun <ihor.kresiun@droid-technologies.com>
2026-01-06 15:18:39 +0200

    Added REPL mode

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include "token.h"
 #include "parser.h"
 
+#include "string.h"
+
 void print_usage(const char* prog_name) {
     printf("Usage: %s \"expression\"\n", prog_name);
     printf("Evaluates the arithmetic expression provided as a command-line argument.\n");
@@ -12,20 +14,32 @@ void print_usage(const char* prog_name) {
 
 int main(int argc, char** argv)
 {
-    if(argc != 2) {
-        print_usage(argv[0]);
-        return 1;
+    char input[256] = {0};
+    static const char* welcome_msg = 
+        "NanoPython Calculator\n"
+        "Type 'exit' or 'quit' to leave.\n";
+
+    printf("%s", welcome_msg);
+
+    static const char* prompt = ">>> ";
+
+    Lexer lexer;
+    Parser parser;
+
+    while (1)
+    {
+        printf("%s", prompt);
+        scanf("%s", input);
+        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
+            break;
+        }
+        
+        lexer = (Lexer){input, 0, input[0]};
+        parser = (Parser){&lexer, lexer_next(&lexer)};
+        Expr* ast = parse_expr(&parser);
+        double result = eval(ast);
+        printf("%g\n", result);
     }
-
-    const char* input = argv[1];
-    printf("Input: %s\n", input);
-
-
-    Lexer lexer = {input, 0, input[0]};
-    Parser parser = {&lexer, lexer_next(&lexer)};
-    Expr* ast = parse_expr(&parser);
-    double result = eval(ast);
-    printf("%g\n", result);
 
     return 0;
 }
